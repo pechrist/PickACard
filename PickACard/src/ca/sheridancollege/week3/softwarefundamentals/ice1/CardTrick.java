70d5344fa8b4cd6156da5b59e2db6640701cef0e
Chris <Chris@DESKTOP-ONCPLCC>
2021-02-14 18:58:46 -0500

    Edited the CardTrick Class twice

--- PickACard/src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ PickACard/src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,6 +5,9 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -16,19 +19,37 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
 public class CardTrick {
     
     public static void main(String[] args)
-    {
+    {   
+        Scanner input = new Scanner(System.in);
+        
         Card[] magicHand = new Card[7];
+        
+        Random random = new Random();
+        
         
         for (int i=0; i<magicHand.length; i++)
         {
+            
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-    }
+        System.out.println("Pick a suit for your card");//insert code to ask the user for Card value and suit, create their card
+        for (int i =0; i< Card.SUITS.length; i++){
+            System.out.println((i + 1)+ ":"+ Card.SUITS[i]);
+        }
+        int suit = input.nextInt();
+        
+        System.out.println("Enter Value (1 to 13");
+        int value = input.nextInt();
+          
+        
+        
+              }
+          
+    
+   
+    
     
 }
